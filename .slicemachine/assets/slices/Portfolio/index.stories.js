ecc6b26f80aed5c19792a01f0dfe6ee5c710afc7
Clayton Schneider <schneiderc2016@gmail.com>
2022-08-01 16:14:19 -0400

    Inital commit

---
+++ .slicemachine/assets/slices/Portfolio/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Portfolio';
+
+export default {
+  title: 'slices/Portfolio'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"portfolio","items":[{"project":{"link_type":"Web","url":"https://slicemachine.dev"}},{"project":{"link_type":"Web","url":"https://prismic.io"}},{"project":{"link_type":"Web","url":"https://prismic.io"}},{"project":{"link_type":"Web","url":"https://prismic.io"}}],"primary":{"title":[{"type":"heading1","text":"Extend scalable relationships","spans":[]}],"description":[{"type":"paragraph","text":"Occaecat aute dolor culpa id culpa esse irure sint aliquip id Lorem velit. Ad aute cupidatat pariatur tempor voluptate pariatur sunt. Incididunt dolore aliqua cupidatat incididunt aliquip in magna elit.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Link from "next/link";
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+import { linkResolver, repositoryName } from "../prismicio";
+
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, ...props }) => (
+        <Link href={href}>
+          <a {...props} />
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
