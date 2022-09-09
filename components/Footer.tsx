4159b0a7f685ac7a5d972912cb611a0e7ace5efc
Clayton Schneider <schneiderc2016@gmail.com>
2022-09-09 16:04:12 -0400

    added noreferrer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div>
       <p className="my-5 text-center text-xs text-gray-400">
         Powered by{" "}
-        <a href="https://simply-sprout.com" target="_blank">
+        <a href="https://simply-sprout.com" target="_blank" rel="noreferrer">
           Simply Sprout
         </a>
       </p>
